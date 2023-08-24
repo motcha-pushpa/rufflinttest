74d520c83a2f688ed5e55e8fa2cba0e1f3881482
motcha-pushpa <59189144+motcha-pushpa@users.noreply.github.com>
2023-08-24 20:55:05 +0530

    test commit2

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ app = FastAPI()
 # test message 23
 @app.get("/")
 async def root():
-    x = make_three_adders()
-    return {"message": str(x)}
+    
+    return {"message": "hello"}
 
-def make_three_adders():
-    result = []
-    for i in [10, 20, 30]:
-        def add(x):
-            return x + i
-        result.append(add)
-    return result
